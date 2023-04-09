768eb3cb339d4272366d223b40e2dac6566de1a4
Aman Singh <amans271999@gmail.com>
2023-04-09 21:13:29 +0530

    fetched news from api

---
+++ src/components/NewsItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export class NewsItem extends Component {
+
+  render() {
+    // object destructuring
+    let {title, description, imageUrl, newsUrl} = this.props;
+
+    return (
+      <div className='my-3'>
+        <div className="card" style={{width: '18rem'}}>
+          <img src={imageUrl} alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target="_blank" className="btn bt-sm btn-primary" rel="noreferrer">Read</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem;
