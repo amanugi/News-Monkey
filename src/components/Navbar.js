e17ec62ddb30d133756a03d66d2b61e5749dd2df
Aman Singh <amans271999@gmail.com>
2023-05-19 13:05:29 +0530

    Modified Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = ()  => {
   
   return (
-      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar fixed-top navbar-expand-lg" style={{ background: 'cadetBlue' }}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">NewsMonkey</Link>
+        <NavLink className="navbar-brand" to="/" style={{ color: 'white' }}>NewsMonkey</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <NavLink className="nav-link " aria-current="page" to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/business">Business</Link>
+              <NavLink className="nav-link" to="/business">Business</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/sports">Sports</Link>
+              <NavLink className="nav-link" to="/sports">Sports</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/technology">Technology</Link>
+              <NavLink className="nav-link" to="/technology">Technology</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/entertainment">Entertainment</Link>
+              <NavLink className="nav-link" to="/entertainment">Entertainment</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/science">Science</Link>
+              <NavLink className="nav-link" to="/science">Science</NavLink>
             </li>
           </ul>
         </div>
